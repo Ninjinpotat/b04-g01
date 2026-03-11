e26c8f3c1d2faa2b493032f296da4da807e558c7
ongmy <ongmy2014@gmail.com>
2026-03-11 12:04:08 +0800

    Initial commit including only Studio12 and alex

---
+++ Studio12/arduino_serial/arduino_serial.ino
@@ -0,0 +1,24 @@
+typedef struct {
+    //char c;
+    //char dummy[3];
+    int32_t x;
+    int32_t y;
+} TData;
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        char ch = (char)Serial.read();
+        if (ch == 's') {
+            TData d;
+            //d.c = 'a';
+            d.x = 100;
+            d.y = 200;
+            Serial.write((uint8_t)sizeof(d));          // first byte: size
+            Serial.write((uint8_t*)&d, sizeof(d));     // the struct itself
+        }
+    }
+}
